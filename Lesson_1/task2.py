70e3f4f23d6cf8fc9c297281b62934197a8e9893
Andrew Baramba <c.crovvhite.w@gmail.com>
2022-10-21 12:09:53 +0300

    has been renamed variables in task 2

--- Lesson_1/task2.py
+++ Lesson_1/task2.py
@@ -1,25 +1,25 @@
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
 
 operation = input("\nChoose operation (+, -, *, /, square, root): ")
 
 if operation == "+":
-    print(str(a) + " + " + str(b) + " = " + str(a + b))
+    print(str(firstNumber) + " + " + str(secondNumber) + " = " + str(firstNumber + secondNumber))
 
 elif operation == "-":
-    print(str(a) + " - " + str(b) + " = " + str(a - b))
+    print(str(firstNumber) + " - " + str(secondNumber) + " = " + str(firstNumber - secondNumber))
 
 elif operation == "*":
-    print(str(a) + " * " + str(b) + " = " + str(a * b))
+    print(str(firstNumber) + " * " + str(secondNumber) + " = " + str(firstNumber * secondNumber))
 
 elif operation == "/":
-    print(str(a) + " / " + str(b) + " = " + str(a / b))
+    print(str(firstNumber) + " / " + str(secondNumber) + " = " + str(firstNumber / secondNumber))
 
 elif operation == "square":
-    print(str(a) + " ^ " + str(b) + " = " + str(a ** b))
+    print(str(firstNumber) + " ^ " + str(secondNumber) + " = " + str(firstNumber ** secondNumber))
 
 elif operation == "root":
-    print(str(b) + "th root of " + str(a) + " = " + str(a ** (1 / b)))
+    print(str(secondNumber) + "th root of " + str(firstNumber) + " = " + str(firstNumber ** (1 / secondNumber)))
 
 else:
     print("Invalid operation value!")
