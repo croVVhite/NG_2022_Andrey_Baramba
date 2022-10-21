d21163febde6bafab5cb9ad88e16b2672ea90785
Andrew Baramba <c.crovvhite.w@gmail.com>
2022-10-21 12:44:52 +0300

    cycles have been replaced by the division remainder operator in task 3

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,24 +1,33 @@
 seconds = int(input("Enter number of seconds: "))
-minutes = 0
-hours = 0
-days = 0
 
-while seconds > 60:
-    seconds -= 60
-    minutes += 1
+buf1 = seconds % 86400
+seconds = seconds - buf1
+days = seconds / 86400
+seconds = buf1
+
+buf2 = seconds % 3600
+seconds = seconds - buf2
+hours = seconds / 3600
+seconds = buf2
+
+buf3 = seconds % 60
+seconds = seconds - buf3
+minutes = seconds / 60
+seconds = buf3
+
 if seconds < 10:
     seconds = "0" + str(seconds)
 
-while minutes > 60:
-    minutes -= 60
-    hours += 1
 if minutes < 10:
+    minutes = '%.0f' % minutes
     minutes = "0" + str(minutes)
+else:
+    minutes = '%.0f' % minutes
 
-while hours > 24:
-    hours -= 24
-    days += 1
 if hours < 10:
+    hours = '%.0f' % hours
     hours = "0" + str(hours)
+else:
+    hours = '%.0f' % hours
 
-print(str(days) + " Day " + str(hours) + ":" + str(minutes) + ":" + str(seconds))
+print(('%.0f' % days) + " Day " + hours + ":" + minutes + ":" + str(seconds))
