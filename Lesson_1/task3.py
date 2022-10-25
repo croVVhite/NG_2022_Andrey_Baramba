095a98076665d976d3b86b85251e16e7049f2d64
Andrew Baramba <c.crovvhite.w@gmail.com>
2022-10-25 21:32:03 +0300

    conditions were replaced with formatted output in task 3

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -2,32 +2,17 @@ seconds = int(input("Enter number of seconds: "))
 
 buf1 = seconds % 86400
 seconds = seconds - buf1
-days = seconds / 86400
+days = int(seconds / 86400)
 seconds = buf1
 
 buf2 = seconds % 3600
 seconds = seconds - buf2
-hours = seconds / 3600
+hours = int(seconds / 3600)
 seconds = buf2
 
 buf3 = seconds % 60
 seconds = seconds - buf3
-minutes = seconds / 60
+minutes = int(seconds / 60)
 seconds = buf3
 
-if seconds < 10:
-    seconds = "0" + str(seconds)
-
-if minutes < 10:
-    minutes = '%.0f' % minutes
-    minutes = "0" + str(minutes)
-else:
-    minutes = '%.0f' % minutes
-
-if hours < 10:
-    hours = '%.0f' % hours
-    hours = "0" + str(hours)
-else:
-    hours = '%.0f' % hours
-
-print(('%.0f' % days) + " Day " + hours + ":" + minutes + ":" + str(seconds))
+print("{0} Days {1:02d}:{2:02d}:{3:02d}".format(days, hours, minutes, seconds))
