ef7fc03189cfbe3baa0f74cd789f909c9dac184f
Andrew Baramba <c.crovvhite.w@gmail.com>
2022-10-27 21:54:24 +0300

    minor changes in task 3

--- Lesson_1/task3.py
+++ Lesson_1/task3.py
@@ -1,18 +1,15 @@
 seconds = int(input("Enter number of seconds: "))
 
 buf1 = seconds % 86400
-seconds = seconds - buf1
-days = int(seconds / 86400)
+days = int((seconds - buf1) / 86400)
 seconds = buf1
 
 buf2 = seconds % 3600
-seconds = seconds - buf2
-hours = int(seconds / 3600)
+hours = int((seconds - buf2) / 3600)
 seconds = buf2
 
 buf3 = seconds % 60
-seconds = seconds - buf3
-minutes = int(seconds / 60)
+minutes = int((seconds - buf3) / 60)
 seconds = buf3
 
 print("{0} Days {1:02d}:{2:02d}:{3:02d}".format(days, hours, minutes, seconds))
