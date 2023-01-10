1cc30a5ba2799a75aecf11ed13214766cb28d8fe
Andrew Baramba <c.crovvhite.w@gmail.com>
2023-01-10 17:10:02 +0200

    removed folders with tasks

--- Lesson_3/task3/task3Funcs.py
+++
@@ -1,7 +0,0 @@
-def calculateLettersAmount(userString, letterAmount):
-    if len(userString) == 1:
-        letterAmount[userString[0]] += 1
-        return letterAmount
-    else:
-        letterAmount[userString[0]] += 1
-        return calculateLettersAmount(userString[1:], letterAmount)
